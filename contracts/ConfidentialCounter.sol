6b4661d9ff6c77f9aba732f7b0eb6f05ec0027d8
WiiUzxz <regelmadz022916@gmail.com>
2025-09-22 19:53:44 +0800

    Update ConfidentialCounter.sol

--- contracts/ConfidentialCounter.sol
+++ contracts/ConfidentialCounter.sol
@@ -1,1 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
+
+contract ConfidentialCounter {
+// Store latest ciphertext per user
+mapping(address => bytes) public latestCipher;
+
+
+event CipherStored(address indexed user, bytes cipher);
+
+
+// Store a ciphertext (the result of FHE evaluation)
+function setCipher(bytes calldata cipher) external {
+latestCipher[msg.sender] = cipher;
+emit CipherStored(msg.sender, cipher);
+}
+
+
+// Read the stored ciphertext for a user
+function getCipher(address user) external view returns (bytes memory) {
+return latestCipher[user];
+}
+}
